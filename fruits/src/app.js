83b3c2813cdc47a358a854ee02ad911cb3315c49
tw <1281773314@qq.com>
2017-04-18 17:19:42 +0800

    add by tw

--- fruits/src/app.js
+++ fruits/src/app.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import router from './router/router.js';
-import App from './App.vue'
-import store from './store/store.js'
-import findStore from './store/findStore.js'
+import App from './App.vue';
+import store from './store/store.js';
+import findStore from './store/findStore.js';
+import twStore from './store/twStore.js';
 new Vue({
 	el: '#app',
 	router,
-	store:{store:store,findStore:findStore}
+	store:{store:store,findStore:findStore,twStore:twStore}
 })
--- fruits/src/components/classify/ClassifyComponent.vue
+++ fruits/src/components/classify/ClassifyComponent.vue
@@ -5,15 +5,15 @@
         <li>
             <h2>加工蔬菜</h2>
             <ul>
-                <li class="pList">
+                <li class="pList" @click="test">
                     <img src="../../../images/like3.jpeg" alt="" />
                     <p>低温酸奶</p>
                 </li>
-                <li class="pList">
+                <li class="pList" @click="test">
                     <img src="../../../images/like4.png" alt="" />
                     <p>低温鲜奶</p>
                 </li>
-                <li class="pList">
+                <li class="pList" @click="test">
                     <img src="../../../images/like9.png" alt="" />
                     <p>中粮猪肉</p>
                 </li>
@@ -28,10 +28,31 @@
 <script type="text/javascript">
 
     import Vue from 'vue';
-    // import VueRouter from 'vue-router'; 
+   import {mapActions} from "vuex";
 
     export default {
-        name: 'ClassifyComponent'
+        name: 'ClassifyComponent',
+        data:function(){
+            return {
+                name: 'tw',
+                pwd: 123456
+            }
+        },
+        methods:{
+            ...mapActions(["test"]),
+            test:function(){
+                try {
+                    var data={
+                        name:this.name,
+                        pwd:this.pwd
+                     }
+                    // console.log(this)
+                     this.$store.twStore.dispatch("hzm",data )
+                 }catch(error) {
+                     alert(error)
+                 }
+            }
+        }
 
     }
 </script>
